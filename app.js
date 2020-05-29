5928b26748602ad413ad0a1d92a34e26836545ef
Natalie-Cardot <nataliecardot@gmail.com>
2020-05-28 18:23:20 -0700

    Parse incoming JSON data

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+// Middleware needed to parse incoming JSON data so can extract it on the request body (body parser adds body field on incoming request)
+// app.use(bodyParser.urlencoded()); // For x-www-form-urlencoded, default format for data sent via form post request
+app.use(bodyParser.json()); // application/json
 
 app.use('/feed', feedRoutes);
 
