d5669d817b28e9c1458c589e36a46e06ca4b106e
Natalie-Cardot <nataliecardot@gmail.com>
2020-05-30 17:25:00 -0700

    Edit comment

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // For x-www-form-urlencoded, default format for data sent via form post request
 app.use(bodyParser.json()); // application/json
 
-// Set CORS headers to bypass CORS error, a default security mechanism set by browsers that occurs when the server-side web API and client are on different servers/domains and try to exchange data
+// Set CORS headers to bypass CORS error, a default security mechanism set by browsers that occurs when the server-side web API (the back end, which has the API endpoints, the path and method, and defines the logic that should execute on the server when a request reaches them) and client (front end) are on different servers/domains and try to exchange data
 app.use((req, res, next) => {
   // Allow data/content to be accessed by specific origins/clients (all in this case)
   res.setHeader('Access-Control-Allow-Origin', '*');
