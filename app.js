16994d0297baa462709132e2fe76cc3a8437a31e
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-03 17:45:56 -0700

    Configure environment variables

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Tells server to load anything in .env into an environment variable.
+require('dotenv').config();
 
 const feedRoutes = require('./routes/feed');
+
+const MONGODB_URI =
+  // process object is globally available in Node app; part of Node core runtime. The env property contains all environment variables known by process object. Using dotenv to store environment variables. It loads environment variables from .env file into process.env (see https://www.youtube.com/watch?v=17UVejOw3zA)
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-4yuid.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
 
 const app = express();
 
@@ -25,4 +33,8 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 app.listen(8080);
