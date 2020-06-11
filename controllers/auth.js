ba783f6bc122186ebc871ff9f8896c6c63df3f42
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-10 22:52:42 -0700

    Implement signup functionality (#387)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
@@ -12,6 +13,26 @@ exports.signup = (req, res, next) => {
     throw error;
   }
   const { email, name, password } = req.body;
+  // Generates hashed password. Asynchronous task; returns a promise. Second arg is salt value (how many rounds of hashing will be applied)
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPw) => {
+      const user = new User({
+        email,
+        password: hashedPw,
+        name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      // Result is user object; can extract userId from it
+      // 201: Created. Request has been fulfilled and resulted in a new resource being created
+      res.status(201).json({ message: 'User created!', userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
-
-exports.signup = (req, res, next) => {};
