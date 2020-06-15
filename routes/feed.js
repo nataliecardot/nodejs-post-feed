97c2dbf493177d21a4ae63292500dd4840f4357d
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-15 08:54:46 -0700

    Add auth middleware to all routes (#392)

--- routes/feed.js
+++ routes/feed.js
@@ -12,6 +12,7 @@ router.get('/posts', isAuth, feedController.getPosts);
 // POST /feed/post
 router.post(
   '/post',
+  isAuth,
   [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 }),
@@ -19,12 +20,13 @@ router.post(
   feedController.createPost
 );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 // Editing a post is like replacing old post with a new one; only keeping old ID; PUT method used for replacing a resource
 // Can use PUT for async requests triggered by JS, but not through normal forms
 router.put(
   '/post/:postId',
+  isAuth,
   [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 }),
@@ -33,6 +35,5 @@ router.put(
 );
 
 // For DELETE routes, can't send body, but as with all routes, can encode data in the URL
-router.delete('/post/:postId', feedController.deletePost);
-
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 module.exports = router;
