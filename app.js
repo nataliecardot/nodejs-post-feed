ee0f277ad03663a6b60f3c93420ea80622761873
Natalie-Cardot <nataliecardot@gmail.com>
2020-06-20 20:19:41 -0700

    Allow OPTIONS method

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
   // Allow these origins to use specific HTTP methods
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE'
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
   );
   // Headers clients can use on requests
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
