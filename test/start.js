1d24204dc78de4f7ccb4be3e2739e042136bf478
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-15 13:46:21 -0700

    Mocha/Chai setup with sample tests

---
+++ test/start.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai');
+
+// mocha is responsible for running tests and providing it() function for defines test code, while chai is responsible for defining success conditions
+
+// when you run npm test, it automatically looks for folder named test and executes all tests, all files, defined within it
+
+it('should add numbers correctly', () => {
+  const num1 = 2;
+  const num2 = 3;
+  expect(num1 + num2).to.equal(5);
+});
+
+it('should not have a result of 6', () => {
+  const num1 = 2;
+  const num2 = 3;
+  expect(num1 + num2).not.to.equal(6);
+});
