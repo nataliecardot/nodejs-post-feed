8d8b3390dc4f5297a6c169e14f40a4c077ccccac
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-16 15:15:37 -0700

    Fix test setup syntax error

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,18 +1,31 @@
 const { expect } = require('chai');
-const sinon = require('sinon')
+const sinon = require('sinon');
 
-const User = require('../models/user')
-const AuthController = require('../controllers/auth')
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
 
 describe('Auth controller - login', () => {
-  if('should throw an error with status code 500 if accessing database fails', () => {
-    sinon.stub(User, 'findOne')
-    // Force an error to be thrown
-    User.findOne.throws();
+  it(
+    ('should throw an error with status code 500 if accessing database fails',
+    () => {
+      sinon.stub(User, 'findOne');
+      // Force an error to be thrown
+      User.findOne.throws();
 
-    // login is an async function (promises used within it); added complexity
-    expect(AuthController.login)
+      const req = {
+        body: {
+          email: 'test@test.com',
+          password: 'tester',
+        },
+      };
 
-    User.findOne.restore()
-  })
-})
+      AuthController.login(req, {}, () => {}).then((result) => {
+        console.log(result);
+        expect(result).to.be.an('error');
+        expect(result).to.have.property('statusCode', 500);
+      });
+
+      User.findOne.restore();
+    })
+  );
+});
