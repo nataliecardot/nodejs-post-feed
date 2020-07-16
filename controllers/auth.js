b29ad31ac8eff8eb4b18ec6fd0ed35c0bd62b57f
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-16 15:10:43 -0700

    Return promise or error depending on success/fail

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,14 +63,15 @@ exports.login = async (req, res, next) => {
       { expiresIn: '1h' }
     );
     res.status(200).json({ token, userId: loadedUser._id.toString() });
+    // Implicitly return promise (as undefined) for use in testing
+    return;
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
+    return err;
   }
-  // Implicitly returns promise hidden behind async/await (for use in auth-controller test file)
-  return;
 };
 
 exports.getUserStatus = async (req, res, next) => {
