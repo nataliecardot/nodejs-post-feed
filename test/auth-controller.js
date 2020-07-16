d0ad6a5783d406e92a3595f44fc55587a6b2ffcb
Natalie-Cardot <nataliecardot@gmail.com>
2020-07-16 14:46:52 -0700

    Add auth controller test setup

---
+++ test/auth-controller.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const sinon = require('sinon')
+
+const User = require('../models/user')
+const AuthController = require('../controllers/auth')
+
+describe('Auth controller - login', () => {
+  if('should throw an error with status code 500 if accessing database fails', () => {
+    sinon.stub(User, 'findOne')
+    // Force an error to be thrown
+    User.findOne.throws();
+
+    // login is an async function; added complexity
+    expect(AuthController.login)
+
+    User.findOne.restore()
+  })
+})
